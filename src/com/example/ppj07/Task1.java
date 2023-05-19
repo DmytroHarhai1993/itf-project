e5566799e78ae6f6f57d686a0715d5ad53796024
dmitrii_gargai <gargai1993@gmil.com>
2023-05-19 22:46:45 +0300

    ppj07 add Task2

---
+++ src/com/example/ppj07/Task1.java
@@ -0,0 +1,41 @@
+package com.example.ppj07;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+     //   task1V1();
+        task1V2();
+    }
+
+    public static void task1V1() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (c < a && c < b) {
+            System.out.println(a + " " + b);
+        } else if (b < a && b < c) {
+            System.out.println(a + " " + c);
+        } else {
+            System.out.println(b + " " + c);
+        }
+    }
+
+    public static void task1V2() {
+        Scanner scanner = new Scanner(System.in);
+        int max1 = scanner.nextInt();
+        int max2 = scanner.nextInt();
+        int temp = scanner.nextInt();
+        // добавить оператор if(max2 > max1);
+        if(temp >= max1){
+            max1 = temp;
+        }else if(temp >= max2){
+            max2 = temp;
+        }
+        System.out.println(max1 + " " + max2);
+        // Написать код:
+        // Переписать переменную temp в max1 или  max2 (или ни в олдну из них) в зависимости от значения.
+    }
+
+}
---
+++ src/com/example/ppj07/Task3.java
@@ -0,0 +1,33 @@
+package com.example.ppj07;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Enter the mileage of the distance for a calculate gallon's value ");
+         session();
+    }
+    public static void session(){
+        final double gallon = 3.7;
+        final String formatUS = "gallon's value by miles ever";
+        final String formatEU = "gallon's value by kilometres ever";
+        System.out.println("Enter the number of the distance: ");
+        int user1 = new Scanner(System.in).nextInt();
+        System.out.println("Enter the format variable: ");
+        String user2 = new Scanner(System.in).nextLine();
+        if(Objects.equals(user2, "km")){
+            System.out.println(gallon/100 * user1 + "  " +  formatEU);
+        }else if(Objects.equals(user2,"ml")){
+            System.out.println((gallon/100 * user1) * 1.6  + "  " + formatUS);
+        }else{
+            System.out.println("Error");
+        }
+    }
+
+
+
+
+
+
+}
