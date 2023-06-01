04fca7ccb01aeecdac48f710c666cc6974fe9a74
dmitrii_gargai <gargai1993@gmil.com>
2023-06-01 17:59:42 +0300

    Renamed int (user1 and user2) to (distanceNumb and DistanceType

--- src/com/example/ppj07/Task3.java
+++ src/com/example/ppj07/Task3.java
@@ -13,13 +13,13 @@ public class Task3 {
         final String formatUS = "gallon's value by miles ever";
         final String formatEU = "gallon's value by kilometres ever";
         System.out.println("Enter the number of the distance: ");
-        int user1 = new Scanner(System.in).nextInt();
+        int distanceNumb = new Scanner(System.in).nextInt();
         System.out.println("Enter the format variable: ");
-        String user2 = new Scanner(System.in).nextLine();
-        if(Objects.equals(user2, "km")){
-            System.out.println(gallon/100 * user1 + "  " +  formatEU);
-        }else if(Objects.equals(user2,"ml")){
-            System.out.println((gallon/100 * user1) * 1.6  + "  " + formatUS);
+        String distanceType = new Scanner(System.in).nextLine();
+        if(Objects.equals(distanceType, "km")){
+            System.out.println(gallon/100 * distanceNumb + "  " +  formatEU);
+        }else if(Objects.equals(distanceType,"ml")){
+            System.out.println((gallon/100 * distanceNumb) * 1.6  + "  " + formatUS);
         }else{
             System.out.println("Error");
         }
